b6f2fe8a9b13bc4a20e75b00439f24b2fc37e648
alygatougozaimasuu <aditamaarsyi@gmail.com>
2025-07-14 11:18:09 +0800

    Update ping-render.js

--- ping-render.js
+++ ping-render.js
@@ -3,4 +3,8 @@ const axios = require("axios");
 
 axios.get(process.env.RENDER_PING_URL)
   .then(() => console.log(`✅ [${new Date().toLocaleString()}] Render Ping Success`))
-  .catch(err => console.error(`❌ [${new Date().toLocaleString()}] Render Ping Failed:`, err.response?.data || err.message));
+  .catch(err => {
+    console.error(`❌ [${new Date().toLocaleString()}] Render Ping Failed`);
+    console.error("Status:", err.response?.status);
+    console.error("Message:", err.message);
+  });
